87978944805405bf1318f2598eaefc8d2bea9e97
kevin.saji <kevin.saji@qburst.com>
2026-04-28 11:02:41 +0530

    spring intialized

---
+++ src/main/java/com/media/mo_poc/MoPocApplication.java
@@ -0,0 +1,13 @@
+package com.media.mo_poc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoPocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoPocApplication.class, args);
+	}
+
+}
