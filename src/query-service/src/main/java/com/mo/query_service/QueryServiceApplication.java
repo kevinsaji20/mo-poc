1eb3f85d0450e8c63179fa8a480e28eecdb8bcaa
kevin.saji <kevin.saji@qburst.com>
2026-04-30 00:28:39 +0530

    Added multi module maven architecture

---
+++ src/query-service/src/main/java/com/mo/query_service/QueryServiceApplication.java
@@ -0,0 +1,13 @@
+package com.mo.query_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QueryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QueryServiceApplication.class, args);
+	}
+
+}
